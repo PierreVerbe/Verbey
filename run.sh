d5af11283928d2c31bbd8047b91af20f744f8083
Pierre Verbe <pierre.verbe@free.fr>
2020-08-27 21:51:08 +0200

    Add script

---
+++ run.sh
@@ -0,0 +1,17 @@
+g++ sources/main.cpp \
+    sources/vs/affichage.cpp \
+    sources/vs/balle.cpp \
+    sources/vs/calculs.cpp \
+    sources/vs/fct_jeu.cpp \
+    sources/vs/gestion_SDL2.cpp \
+    sources/vs/grenade.cpp \
+    sources/vs/init_donnees.cpp \
+    sources/vs/balle.cpp \
+    sources/vs/manette.cpp \
+    sources/vs/mine.cpp \
+    sources/bib_sdl.cpp \
+    sources/calculs.cpp \
+    -o verbey
+
+./verbey.exe
+sleep 10
